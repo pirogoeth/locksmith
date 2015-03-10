1c30c2a5b7bdb9e16e581180e3be2173f9098cdc
pirogoeth <pirogoeth@maio.me>
2015-03-10 15:26:08 -0500

    Updated url() calls in templates to fix a routing error.

--- locksmith/urls.py
+++ locksmith/urls.py
@@ -1,8 +1,9 @@
-import os
+import os, locksmith
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
+from locksmith import views
 from locksmith.api_v1 import (UserResource, CredentialGroupResource,
     CredentialResource)
 
@@ -15,8 +16,8 @@ api_v1.register(CredentialGroupResource())
 api_v1.register(CredentialResource())
 
 urlpatterns = patterns('',
-    url(r'^$', 'locksmith.views.index', name='index'),
-    url(r'^about/$', 'locksmith.views.about', name='about'),
+    url(r'^$', locksmith.views.index, name='index'),
+    url(r'^about/$', locksmith.views.about, name='about'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(api_v1.urls)),
     url(r'^api/login', 'locksmith.api_v1.api_login'),
--- locksmith/views.py
+++ locksmith/views.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
